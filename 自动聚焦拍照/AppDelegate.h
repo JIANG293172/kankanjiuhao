013d6cbc531937c5e430401876016c8c742cb594
JIANG293172 <tao@taodeimac.local>
2016-12-30 14:34:18 +0800

    Initial Commit

---
+++ 自动聚焦拍照/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自动聚焦拍照
+//
+//  Created by tao on 16/12/30.
+//  Copyright © 2016年 tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自动聚焦拍照/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  自动聚焦拍照
+//
+//  Created by tao on 16/12/30.
+//  Copyright © 2016年 tao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
